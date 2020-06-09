fe3c58145b26bd5eef5a842d31cd01c7b23d447e
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-06-09 09:04:39 +0200

    Finish initializing - Project ready to start

---
+++ public/front/js/app.js
@@ -0,0 +1,171 @@
+import {settings, select, classNames} from './settings.js';
+import Product from './components/Product.js';
+import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
+import Main from './components/Main.js';
+
+const app = {
+  initPages: function(){
+    const  thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    //console.log('pageMatchingHash', pageMatchingHash);
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with thath id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+
+      });
+    }
+
+
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching*/
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+
+    /* add class "active" to matching links, remove from non-matching*/
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+
+  },
+
+  initMainPage: function() {
+    const thisApp = this;
+
+    const mainElem = document.querySelector(select.containerOf.main);
+
+    thisApp.main = new Main(mainElem);
+
+    const elem = document.querySelector('.carousel-content');
+    // eslint-disable-next-line no-undef
+    new Flickity('.carousel-content', {
+      autoPlay: 3000
+    });
+    
+    // eslint-disable-next-line no-undef
+    new Flickity(elem, {
+      cellAlign: 'left',
+      contain: true
+    });
+
+
+  },
+
+  initMenu: function(){
+    const thisApp = this;
+    //console.log('thisApp.data:', thisApp.data);
+
+    for(let productData in thisApp.data.products){
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+  },
+
+  initCart: function(){
+    const thisApp = this;
+
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+
+    thisApp.productList = document.querySelector(select.containerOf.menu);
+
+    thisApp.productList.addEventListener('add-to-cart', function(event){
+      app.cart.add(event.detail.product);
+    });
+  },
+
+  initData: function(){
+    const thisApp = this;
+
+    thisApp.data = {};
+    const url = settings.db.url + '/' + settings.db.product;
+
+    fetch(url)
+      .then(function(rawResponse){
+        return rawResponse.json();
+      })
+      .then(function(parsedResponse){
+        console.log('parsed response', parsedResponse);
+
+        /* Save parsedResponse as thisApp.data.products */
+        thisApp.data.products = parsedResponse;
+
+        /* execute initMenu method */
+        thisApp.initMenu();
+      });
+      
+    //console.log('thisApp.data', JSON.stringify(thisApp.data));
+      
+  },
+
+  initBooking: function(){
+    const thisApp = this;  
+
+    const bookingWidget = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingWidget);
+
+  },
+
+
+
+  init: function(){
+    const thisApp = this;
+    //console.log('*** App starting ***');
+    //console.log('thisApp:', thisApp);
+    //console.log('classNames:', classNames);
+    //console.log('settings:', settings);
+    //console.log('templates:', templates);
+
+    thisApp.initPages();
+    thisApp.initData();
+    thisApp.initCart();
+    thisApp.initBooking();
+    thisApp.initMainPage();
+
+  },
+
+};
+
+window.addEventListener('hashchange', function() {
+  const idFromHash = window.location.hash.replace('#/', '');
+  app.activatePage(idFromHash);
+});
+
+
+app.init();
+
+
--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React :D
+          Learn React :D xd
         </a>
       </header>
     </div>
