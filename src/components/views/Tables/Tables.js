dc0449679c184ec73ab332ab0c09db8723b49847
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-07-04 21:37:41 +0200

    Add view (v2) + routing

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import styles from './Tables.module.scss';
+import { Link } from 'react-router-dom';
 
 
 const Tables = () => {
   return (
     <div className={styles.component}>
       <h2>Tables view</h2>
+      <li>
+        <Link to={process.env.PUBLIC_URL + '/tables/booking/:id'}>Check tables id </Link>
+      </li>
+      <li>
+        <Link to={process.env.PUBLIC_URL + '/tables/booking/new'}>Check new booking</Link>
+      </li>
+      <li>
+        <Link to={process.env.PUBLIC_URL + '/tables/events/:id'}>Check events id</Link>
+      </li>
+      <li>
+        <Link to={process.env.PUBLIC_URL + '/tables/events/new'}>Check for new events</Link>
+      </li>
+
     </div>
   );
 };
