e3bd7b13aec7679b223b990202fc8980e1e19b64
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-07-05 18:11:56 +0200

    [SOLVED] Add Material UI

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -3,7 +3,6 @@ import styles from './PageNav.module.scss';
 import { NavLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
-
 const PageNav = () => {
   return(
 
@@ -14,7 +13,6 @@ const PageNav = () => {
       <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/waiter`} activeClassName='active'>Waiter </Button>
       <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</Button>
     </nav>
-
   );
 };
 
