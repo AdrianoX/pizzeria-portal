7f5635321b508d1263961a62fd41b0c75174b251
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-07-09 18:32:21 +0200

    Add TableEvents/TablesBooking view - small issues with display

--- src/components/views/TableEvents/TableEvents.js
+++ src/components/views/TableEvents/TableEvents.js
@@ -1,12 +1,96 @@
+
 import React from 'react';
 import styles from './TableEvents.module.scss';
+import { useParams } from 'react-router-dom';
 
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
+const dateInfoTest = {
+  tableID: '9',
+  ppl: '5',
+  date: '2020-05-17',
+  time: '16:15',
+  name: 'Adrian AdrianoX',
+  phone: '666-666-666',
+  smokingTable: 'Yes',
+};
 
 const TableEvents = () => {
+  const {id} = useParams();
   return (
-    <div className={styles.component}>
-      <h2>TableEvents view</h2>
-    </div>
+    <Paper className={styles.component}>
+      <div className={styles.header}>
+        <h2>Events reservation</h2>
+        <h3>Nr: {id}</h3>
+      </div>
+      <Table className={styles.tableID}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Table</TableCell>
+            <TableCell>Amount of ppl</TableCell>
+            <TableCell>Date</TableCell>
+            <TableCell>Hour</TableCell>
+            <TableCell>Phone</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Smoking Table</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell component="th" scope="row">
+              {dateInfoTest.tableID}
+            </TableCell>
+            <TableCell>
+              {dateInfoTest.ppl}
+            </TableCell>
+            <TableCell>
+              <TextField
+                id="date"
+                type="date"
+                value={dateInfoTest.date}
+                className={styles.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </TableCell>
+            <TableCell>
+              <TextField
+                id="time"
+                type="time"
+                value={dateInfoTest.time}
+                className={styles.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </TableCell>
+            <TableCell>
+              {dateInfoTest.phone}
+            </TableCell>
+            <TableCell>
+              {dateInfoTest.name}
+            </TableCell>
+            <TableCell>
+              {dateInfoTest.smokingTable}
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <div className={styles.edit}>
+        <Button>Edit</Button>
+      </div>
+    </Paper>
+
   );
 };
 
